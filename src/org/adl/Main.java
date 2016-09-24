775fe60e77aa9a6f2f69cef08cf31dca4d6946d9
avhimkov <avhimkov@mail.ru>
2016-09-24 22:04:09 +0500

    Win 1251 fix error

--- src/org/adl/Main.java
+++ src/org/adl/Main.java
@@ -1,31 +1,31 @@
 package org.adl;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
 
     public static void main(String[] args) {
-        File myFile = new File("2016-09-22-com.txt");
-
-        Pattern myPat = Pattern.compile("#ONLINE");
-
         try{
-            Scanner myScan = new Scanner(myFile);
+            File myFile = new File("D:\\2016-09-22-com.txt");
+
+            Pattern myPat = Pattern.compile("#ONLINE");
+            Scanner myScan = new Scanner(myFile, "windows-1251");
 
             while(myScan.hasNext()){
+
                 String line = myScan.nextLine();
                 if (myPat.matcher(line).find()) {
                     System.out.println(line);
                 }
+
             }
 
-        }catch(Exception ignored){
+        }catch(Exception e){
 
         }
+
 
 
     }
