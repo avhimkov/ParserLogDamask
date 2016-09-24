899eb107fc81a8925d55ada94ff80fdc7c0ddd4e
avhimkov <avhimkov@mail.ru>
2016-09-24 22:43:30 +0500

    add FindString method

--- src/org/adl/Main.java
+++ src/org/adl/Main.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
+    String put = null;
+    //"(#OFFLINE)(#ONLINE)"
+    String regexp = null;
 
     public static void main(String[] args) {
-        try{
-            File myFile = new File("D:\\2016-09-22-com.txt");
+        FindString("D:\\2016-09-22-com.txt", "#OFFLINE");
+        FindString("D:\\2016-09-22-com.txt", "#ONLINE");
+    }
 
-            Pattern myPat = Pattern.compile("#ONLINE");
+    static void FindString(String put, String regexp) {
+        try {
+            File myFile = new File(put);
+
+            Pattern myPat = Pattern.compile(regexp);
             Scanner myScan = new Scanner(myFile, "windows-1251");
 
-            while(myScan.hasNext()){
+            while (myScan.hasNext()) {
 
                 String line = myScan.nextLine();
                 if (myPat.matcher(line).find()) {
@@ -22,11 +30,11 @@ public class Main {
 
             }
 
-        }catch(Exception e){
+        } catch (Exception e) {
 
         }
-
-
-
     }
 }
+
+
+
