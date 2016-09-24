4d7573da5f7859f45c2764292a6cc44074f18dc8
avhimkov <avhimkov@mail.ru>
2016-09-24 23:02:29 +0500

    if else ramification

--- src/org/adl/Main.java
+++ src/org/adl/Main.java
@@ -5,20 +5,17 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-    String put = null;
-    //"(#OFFLINE)(#ONLINE)"
-    String regexp = null;
 
     public static void main(String[] args) {
-        FindString("D:\\2016-09-22-com.txt", "#OFFLINE");
-        FindString("D:\\2016-09-22-com.txt", "#ONLINE");
+        FindString("D:\\2016-09-22-com.txt", "#OFFLINE", "#ONLINE");
     }
 
-    static void FindString(String put, String regexp) {
+    static void FindString(String put, String regexp, String regexp1) {
         try {
             File myFile = new File(put);
 
             Pattern myPat = Pattern.compile(regexp);
+            Pattern myPat1 = Pattern.compile(regexp1);
             Scanner myScan = new Scanner(myFile, "windows-1251");
 
             while (myScan.hasNext()) {
@@ -27,6 +24,13 @@ public class Main {
                 if (myPat.matcher(line).find()) {
                     System.out.println(line);
                 }
+                else  {
+
+                    if (myPat1.matcher(line).find()) {
+                        System.out.println(line);
+                    }
+                }
+
 
             }
 
