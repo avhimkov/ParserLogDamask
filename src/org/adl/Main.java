f75c5f135e3771408fe6611d7c24c42cbc91045b
avhimkov <avhimkov@mail.ru>
2016-10-08 15:14:32 +0500

    Refactoring streams

--- src/org/adl/Main.java
+++ src/org/adl/Main.java
@@ -1,9 +1,14 @@
 package org.adl;
 
 import java.io.*;
-import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 import java.util.regex.Pattern;
 import java.lang.String;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     static boolean flag;
@@ -31,11 +36,12 @@ public class Main {
             Scanner myScan = new Scanner(myFile, "windows-1251");
             myScan.hasNext();
             String line = myScan.nextLine();
+
             String ffile = findFile(line, data);//"2016-09-22-com.txt"
 
 //          вызов функции для поиска строк
 
-            findString(ffile, /*data,*/ nomerWindow); //"#OFFLINE", "#ONLINE", "#KEY_OFF_PRESSED", "#KEY_ON_PRESSED"
+            findString(ffile, nomerWindow); //"#OFFLINE", "#ONLINE", "#KEY_OFF_PRESSED", "#KEY_ON_PRESSED"
 
         } catch (StringIndexOutOfBoundsException e) {
             System.out.println("Неправельно введена дата. Введите дату в формате DD.MM.EEEE (пример: 01.01.2016)");
@@ -43,22 +49,31 @@ public class Main {
     }
 
     //Вывод найденные строки
-    static void findString(String put, /*String data,*/ String numberWindow) {
-        try {
-            InputStream myFile = new BufferedInputStream(new FileInputStream(put));
-            Pattern nomerOkna = Pattern.compile(numberWindow);
-//            Pattern dataFile = Pattern.compile(data);
-            Scanner myScan = new Scanner(myFile, "windows-1251");
+    static void findString(String put, String numberWindow) throws IOException {
+        List<String> list = new ArrayList<>();
+//        try (Stream<String> stream = Files.lines(Paths.get(put))){
+//            list = stream
+//                    .filter(line -> line.matches(numberWindow))
+//                    .collect(Collectors.toList());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        list.forEach(System.out::println);
+
+        InputStream myFile = new BufferedInputStream(new FileInputStream(put));
+        Pattern nomerOkna = Pattern.compile(numberWindow);
+        Scanner myScan = new Scanner(myFile, "windows-1251");
+
+
 //            проход по строка и поиск згачений
+
             while (myScan.hasNext()) {
                 String line = myScan.nextLine();
-//                if (dataFile.matcher(line).find()){
-//                    System.out.println(line);
-//                }
-                if (nomerOkna.matcher(line).find()){
+
+                if (nomerOkna.matcher(line).find()) {
                     System.out.println(line);
                 }
-
+            }
 //                String[] substr = line.split(" ");
 //                вывод строк в другом порядеке
 //                if (online.matcher(line).find()) {
@@ -85,12 +100,9 @@ public class Main {
 //                    String name = substr[substr.length-1];
 //                    System.out.println("Время: " + time1 + " " + "\033[31m" + okno + "\033[m" + " Имя " + "\033[32m" + name + "\033[m" +  " Нажата кнопка включить");
 //                }
-            }
-        } catch (FileNotFoundException e) {
-            System.out.println("Фаил не найден");
-//            e.printStackTrace();
-        }
+//    }
     }
+
     //  поиск файла в директории
     static String findFile(String path, String find) {
         File f = new File(path);
